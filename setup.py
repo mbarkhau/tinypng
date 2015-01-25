ad45faaf7c9ed7d00bc7ceb77d805ae655168d85
Manuel Barkhau <mbarkhau@gmail.com>
2015-01-25 20:29:42 +0100

    add keywords becaus now jpeg compatible

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         [console_scripts]
         tinypng=tinypng:main
     """,
-    keywords="png image compression tinypng shrink",
+    keywords="png image compression tinypng shrink jpeg jpg",
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
